db749e2181891177cc492a154989db4808d9c622
Dieggo0 <119446059+Dieggo0@users.noreply.github.com>
2023-05-16 14:48:08 -0300

    componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BodyComponent } from './components/body/body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
